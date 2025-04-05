c63dadb0664b6fce0ec2ac4f5893861d710b7607
Oluwa-Laughter <ismakinde@gmail.com>
2025-04-05 19:33:31 +0100

    modern-javascript

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,57 @@
-let age;
-const btn = document.querySelector("button")!;
+// const userName = "Isaac";
+// let age = 50;
 
-function add(a: number, b: number) {
-  if (a + b > 0) return a + b;
-  return;
-}
+// age = 25;
 
-function clickHandler(message: string) {
-  let num = 0;
-  console.log("clicked " + message);
-}
+// function add(a: number, b: number) {
+//   var result;
+//   result = a + b;
 
-// comment
+//   return result;
+// }
+
+// console.log(result);
+
+// if (age > 20) {
+//   let isOld = true;
+// }
+
+// console.log(isOld);
+
+const add2 = (a: number, b: number = 2) => a + b;
+
+const printOutput: (a: string | number) => void = (output) =>
+  console.log(output);
+
+const btn = document.querySelector("button");
+
 if (btn) {
-  btn.addEventListener("click", clickHandler.bind(null, "You're welcome!"));
+  btn.addEventListener("click", (event) => console.log(event));
 }
+
+printOutput(add2(4));
+
+const hobbies = ["Sport", "cooking"];
+const activeHobbies = ["Hiking"];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+  firstName: "Isaac",
+  age: 40,
+};
+
+const copiedPerson = { ...person };
+
+const add3 = (...numbers: number[]) => {
+  return numbers.reduce((acc, cur) => acc + cur, 0);
+};
+
+const addedNumbers = add3(6, 6, 3, 6, 8);
+console.log(addedNumbers);
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+console.log(hobbies, hobby1, hobby2);
+
+const { firstName: userName, age } = person;
+console.log(userName, age);
