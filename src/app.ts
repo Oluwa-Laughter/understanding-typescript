b07df9e445b35eabd15328087526c6991ba70798
Oluwa-Laughter <ismakinde@gmail.com>
2025-04-06 19:19:54 +0100

    private & public access, readonly properties, inheritance

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,68 @@
 class Department {
-  constructor(n: string) {
-    this.name = n;
+  // private readonly id: string;
+  // private name: string;
+  private employees: string[] = [];
+
+  constructor(private readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = name;
   }
 
-  name: string;
-
   describe(this: Department) {
-    console.log(` ${this.name}`);
+    console.log(` ${this.id} : ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    // this.id = 'u72'
+    this.employees.push(employee);
+  }
+
+  printEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department("Accounting");
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
 
-accounting.describe();
+    this.admins = admins;
+  }
+}
 
-const accountingCopy = { name: "Dummy", describe: accounting.describe };
-accountingCopy.describe();
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+  }
+
+  addReports(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment("52tw", ["Isaac"]);
+
+it.addEmployee("Isaac");
+it.addEmployee("Idan");
+
+it.describe();
+it.printEmployeeInfo();
+
+console.log(it);
+
+const accounting = new AccountingDepartment("acc", []);
+
+accounting.addReports("Money deposited");
+accounting.printReports();
+
+console.log(accounting);
+
+// const accountingCopy = { name: "Dummy", describe: accounting.describe };
+
+// accountingCopy.describe();
